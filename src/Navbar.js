abff7610caaf5c4dc67b8623683fe3a09120c6ad
mpinoh <alvingarrin@gmail.com>
2023-11-19 18:30:40 +0700

    dark/light mode but without react hooks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,61 @@
-import React, { useState , useEffect} from 'react';
+// import React, { useState , useEffect} from 'react';
 import { Link } from 'react-router-dom';
+// import './css/light-mode.css'
+// import './css/dark-mode.css'
+// import './css/light-mode.css'
 
 const Navbar = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    // const [isDarkMode, setIsDarkMode] = useState(false);
 
-    useEffect(() => {
-        const mode = isDarkMode ? 'dark-mode.css' : 'light-mode.css';
-        import(`./css/${mode}`) 
-          .then(() => {
-            console.log(`Successfully imported ${mode}`);
-            console.log('isDarkMode is ' + isDarkMode);
-          })
-          .catch((error) => {
-            console.error('Error importing CSS file:', error);
-          });
-    }, [isDarkMode]);
+    // useEffect(() => {
+    //     (async () => {
+    //         const mode = isDarkMode ? 'dark-mode.css' : 'light-mode.css';
+    //         try {
+    //             const module = await import(`./css/${mode}`);
+    //             console.log(`Successfully imported ${mode}`);
+    //             console.log('isDarkMode is ' + isDarkMode);
+    //         } catch (error) {
+    //             console.error('Error importing CSS file:', error);
+    //         }
+    //     })();
+    // }, [isDarkMode]);
+    
+    // useEffect(() => {
+    //     const mode = isDarkMode ? 'dark-mode.css' : 'light-mode.css';
+    //     import(`./css/${mode}`) 
+    //       .then(() => {
+    //         console.log(`Successfully imported ${mode}`);
+    //         console.log('isDarkMode is ' + isDarkMode);
+    //       })
+    //       .catch((error) => {
+    //         console.error('Error importing CSS file:', error);
+    //       });
+    // }, [isDarkMode]);
 
     const toggleMode = () => {
-        setIsDarkMode((prevMode) => !prevMode);
-        
+        const theme = document.getElementById('theme');
+        const currentTheme = theme.getAttribute('href');
+
+        if (currentTheme === 'light-mode.css') {
+            theme.setAttribute('href', 'dark-mode.css');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            theme.setAttribute('href', 'light-mode.css');
+            localStorage.setItem('theme', 'light');
+        }
     };
+
+    // Check local storage for saved theme and apply it
+    document.addEventListener('DOMContentLoaded', function () {
+        const savedTheme = localStorage.getItem('theme');
+        const theme = document.getElementById('theme');
+        
+        if (savedTheme === 'dark') {
+            theme.setAttribute('href', 'dark-mode.css');
+        } else {
+            theme.setAttribute('href', 'light-mode.css');
+        }
+    });
 
     return ( 
         <nav className="navbar">
@@ -29,7 +65,6 @@ const Navbar = () => {
             <div className="links">
                 <div className="dropdown">
                     <button className="dropbtn">Simple Apps
-                        <i className="fa fa-caret-down"></i>
                     </button>
                     <div className="dropdown-content">
                         <Link to="/bmicalculator">BMI Calculator</Link>
@@ -38,7 +73,6 @@ const Navbar = () => {
                 </div>
                 <div className="dropdown">
                     <button className="dropbtn">Blogs
-                        <i className="fa fa-caret-down"></i>
                     </button>
                     <div className="dropdown-content">
                         <Link to="/blogspage">Blogs Page</Link>
@@ -50,7 +84,7 @@ const Navbar = () => {
                 </div>
                 <div className="dropdown">
                     <button className='button-global' onClick={toggleMode}>
-                        {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+                        {/* {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'} */}Light/Dark mode
                     </button>
                 </div>
             </div>
